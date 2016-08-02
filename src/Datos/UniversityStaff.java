70a6bc87b6430d16057233174eec39e5193b2ece
B106 PC-11 <B106 PC-11@172.30.15.107>
2016-08-02 10:59:27 -0500

    Agregue persistencia

--- src/Datos/UniversityStaff.java
+++ src/Datos/UniversityStaff.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Datos;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author B106 PC-11
+ * @author Nicolas
  */
 public class UniversityStaff {
     
